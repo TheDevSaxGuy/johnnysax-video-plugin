a4a802888a41323d1d3106ebf6b92f27bdc85fb4
unknown <juanitortz97@gmail.com>
2021-11-08 23:01:14 -0600

    Updating landing page

--- assets/index.ts
+++ assets/index.ts
@@ -1,14 +1,14 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
-import Ads from './plugins/Ads'
+//import Ads from './plugins/Ads'
 
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video ,plugins:[
     new AutoPlay(),
     new AutoPause(),
-    new Ads()
+    //new Ads()
 ]});
 
 const playButton:HTMLElement = document.querySelector('#play');
@@ -17,8 +17,9 @@ const muteButton:HTMLElement = document.querySelector('#mute');
 playButton.onclick = () =>{ player.togglePlay();};
 muteButton.onclick=()=>{player.toggleMute();}
 
+/* TEMPORARY DISABLED DUE TO A PROBLEM WITH SERVICE WORKERS
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('../sw.js').catch(error =>{
         console.log(error.message)
     });
-}
+}*/
